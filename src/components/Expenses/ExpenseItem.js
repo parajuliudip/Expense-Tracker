5273ec9d7b0d8f79f566a9057f42c9511ae20454
Udip Parajuli <udip@Udips-MacBook-Air.local>
2021-09-21 14:45:46 +0545

    Arraging the components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,20 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!");
+  };
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
