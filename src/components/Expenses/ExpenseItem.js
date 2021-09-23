9c70f4048617b112facdd0c50024ec58fea1a3af
parajuliudip <udipparajuli17@gmail.com>
2021-09-23 17:12:19 +0545

    Added a form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  // //inside a function we call a useState
+  // // inside the function and any nested function, should call
+  // const [initalvalue, updatevalue] = useState(); array distructing
+
+  const [title, settitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
+
   const clickHandler = () => {
+    settitle("Hello!");
     console.log("Clicked!!");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
